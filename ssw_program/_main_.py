63cb2b5333252cf5f5c54227f01acebec402d0e6
DavidG-123 <57122183+DavidG-123@users.noreply.github.com>
2021-10-04 19:57:47 -0700

    Update _main_.py

--- ssw_program/_main_.py
+++ ssw_program/_main_.py
@@ -3,13 +3,14 @@ from alma import print_alma
 from emma import print_emma
 from josh import print_josh
 from ana import print_ana
-
+from david import printDavid
 
 print_welcome()
 print_alma()
 print_emma()
 print_josh()
 print_ana()
+printDavid()
 
 
 # import os
