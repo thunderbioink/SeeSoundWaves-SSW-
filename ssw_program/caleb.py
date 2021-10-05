c813c2e587e70f1af2bc63247dafa4a06d2b71c7
Caleb Salyards <34790708+Psymatics@users.noreply.github.com>
2021-10-04 20:56:53 -0600

    Adding a dash of snark.

---
+++ ssw_program/caleb.py
@@ -0,0 +1,6 @@
+class caleb:
+    def __init__(self):
+        self.catchphrase = "Caleb is ready to work." # and is such a total over-achiever...
+
+    def introducer(self):
+        print(self.catchphrase)
