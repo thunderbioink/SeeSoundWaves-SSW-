7bf3edddc838225de1c48b2e10bf8ee146774a19
Alma Camarillo <allealle0303@gmail.com>
2021-10-06 15:08:01 -0600

    commit wav files

--- ssw_program/_main_.py
+++ ssw_program/_main_.py
@@ -14,9 +14,3 @@ print_ana()
 printDavid()
 
 
-import os
-print('current directory')
-print(os.getcwd())
-
-print(1*2)
-print(2**3)
--- ssw_program/alma.py
+++ ssw_program/alma.py
@@ -2,3 +2,66 @@ def print_alma():
         print("Hello!This is Alma Camarillo! ")
     
     # python hello_program
+    
+    
+
+
+# import os
+# print('current directory')
+# print(os.getcwd())
+
+print(1*2)
+print(2**3)
+
+
+# import wave, struct
+
+# wave_file = wave.open('..\ssw_program\wav_files\Blue_UnmasteredWAV.wav', 'r')
+
+# length = wave_file.getnframes()
+# for i in range(0, length):
+#     wave_data= wave_file.readframes(48000)
+#     data = struct.unpack('<48000h', wave_data) 
+    
+#     print(int(data[0]))
+
+
+
+# import soundfile as sf
+
+# data, samplerate = sf.read('../wav_files/Blue_UnmasteredWAV.wav')
+# from scikits import audiolab
+# from scipy.io import wavfile
+# from sys import argv
+# for filepath in argv[1:]:
+#     x, fs, nb_bits = audiolab.wavread(filepath)
+#     print('Reading with scikits.audiolab.wavread:', x)
+#     fs, x = wavfile.read(filepath)
+#     print('Reading with scipy.io.wavfile.read:', x)
+
+
+""" Import Modules and packages"""
+
+
+
+# import librosa as lr
+
+
+# import matplotlib
+
+# matplotlib.__version__
+# Test if matplotlib installes after pip install. No error message. Successful
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from glob import glob
+
+import librosa as lr
+
+# continue here
+
+
+
+
