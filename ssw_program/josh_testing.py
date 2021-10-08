c0dd3d135c878a7aefd07ea581245c30e503159c
JoshZ520 <91156062+JoshZ520@users.noreply.github.com>
2021-10-07 19:12:52 -0600

    almost there

--- ssw_program/josh_testing.py
+++ ssw_program/josh_testing.py
@@ -1,4 +1,15 @@
 # Just testing things out to get the video/ img to show up in python
 
 import numpy as np
-import cv2
+import cv2 as cv
+
+cap = cv2.VideoCapture('videos/wa.avi')
+while(cap.isOpened()):
+  ret, frame = cap.read()
+  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  cv2.imshow('frame',gray)
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+    break
+
+cap.release()
+cv2.destroyAllWindows()
